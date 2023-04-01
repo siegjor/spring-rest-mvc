a82b2a0b7503cf29286fddcc222341e2ee77db69
Gabriel <bristotgl@gmail.com>
2023-04-01 18:27:39 -0300

    first commit

---
+++ src/main/java/siegjor/springrestmvc/SpringRestMvcApplication.java
@@ -0,0 +1,14 @@
+package siegjor.springrestmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringRestMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestMvcApplication.class, args);
+	}
+
+}
