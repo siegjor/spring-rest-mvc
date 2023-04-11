3b48c4daa4e204247154c7789df10a9d9f6ade22
Gabriel <bristotgl@gmail.com>
2023-04-10 22:04:45 -0300

    adds ExceptionController

--- src/main/java/siegjor/springrestmvc/controller/BeerController.java
+++ src/main/java/siegjor/springrestmvc/controller/BeerController.java
@@ -57,11 +57,7 @@ public class BeerController {
         return beerService.listBeers();
     }
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity handleNotFoundException() {
-        log.debug(">>> NOT FOUND from custom exception");
-        return ResponseEntity.notFound().build();
-    }
+
 
     @GetMapping(BEER_PATH_ID)
     public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
