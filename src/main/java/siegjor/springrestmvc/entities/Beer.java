957660706ee1f0daf9eb954e7dabf305a4081d2d
Gabriel <bristotgl@gmail.com>
2023-04-11 23:32:21 -0300

    adds beer and costumer entities

---
+++ src/main/java/siegjor/springrestmvc/entities/Beer.java
@@ -0,0 +1,33 @@
+package siegjor.springrestmvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+import siegjor.springrestmvc.model.BeerStyle;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Beer {
+    @Id
+    private UUID id;
+
+    @Version
+    private Integer version;
+
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
---
+++ src/main/java/siegjor/springrestmvc/entities/Customer.java
@@ -0,0 +1,26 @@
+package siegjor.springrestmvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    private UUID id;
+    private String customerName;
+
+    @Version
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
